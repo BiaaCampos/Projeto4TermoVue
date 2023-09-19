5260780f114e6cccbf805477584158f21501a2cd
BiaaCampos <biancavitoriacampos@hotmail.com>
2023-09-19 17:38:07 -0300

    Tela inicial quase completa, falta apenas alguns detalhes e consumir a API escolhida

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+let num = ref(0);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <nav
+    class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body"
+  >
+    <div class="container-fluid">
+      <routerLink to="/" class="navbar-brand">Star Wars</routerLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item"><routerLink to="/" class="nav-link" aria-current="page">Home</routerLink></li>
+          <li class="nav-item"><routerLink to="/about" class="nav-link">About</routerLink></li>
+        </ul>
+      </div>
     </div>
-  </header>
+  </nav>
 
-  <main>
-    <TheWelcome />
-  </main>
+<div class="main">
+  <router-view></router-view>
+</div>
+
+<footer class="bg-dark text-light">
+  <div>Unimar | ADS | {{ new Date().getFullYear() }}</div>
+</footer>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
 }
 </style>
